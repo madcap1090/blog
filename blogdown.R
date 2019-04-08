6275f53fd37dbb96113eec122f7c2bdb7f646f66
madcap1090 <madcap1090@gmail.com>
2019-04-08 21:07:08 +0200

    blogdown file

---
+++ blogdown.R
@@ -0,0 +1,51 @@
+# https://bookdown.org/yihui/blogdown/
+# https://www.garrickadenbuie.com/blog/blogdown-netlify-new-post-workflow/
+# https://app.netlify.com/sites/trusting-swanson-3551ea/overview
+# https://bookdown.org/yihui/blogdown/custom-layouts.html
+library(blogdown) # devtools::install_github("rstudio/blogdown")
+# blogdown::new_site()
+
+# blogdown::install_hugo()
+
+blogdown::serve_site()
+
+
+blogdown::new_post("test2")
+
+file.edit("~/.Rprofile")
+
+#devtools::install_github("hadley/emo")
+library(emo)
+#install.packages("purrr", "Rcpp")
+
+
+# worflow and nice theme
+# https://www.garrickadenbuie.com/blog/blogdown-netlify-new-post-workflow/
+# theme = "hyde"
+# https://github.com/spf13/hyde
+
+
+# minimal themes
+# https://themes.gohugo.io/hugo_theme_pickles/
+
+
+# https://themes.gohugo.io//theme/temple/
+
+# https://themes.gohugo.io/hugo-theme-basic/
+# https://themes.gohugo.io/hugo-xmin/
+# https://themes.gohugo.io/theme/hugo-coder/
+
+
+
+
+
+
+
+# nice themes to test
+# © 2019 Dr. Jason Timm. Powered by Hugo. Theme by PPOffice.
+# https://www.jtimm.net/2018/01/30/keyphrase-extraction-from-a-corpus-of-texts/
+
+# https://themes.gohugo.io/hugo-icarus/
+
+# https://www.r-bloggers.com/setting-up-our-blog-with-rstudio-and-blogdown-i-creating-the-blog/amp/
+
