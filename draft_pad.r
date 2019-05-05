6705939128ea68a8dc9506d86d1e02026d11d987
madcap1090 <madcap1090@gmail.com>
2019-05-05 15:04:44 +0200

    rendered tweets

--- draft_pad.r
+++ draft_pad.r
@@ -1,32 +1,7 @@
+test <- tweets$text
+unique(Encoding(test))
 
+unknown <- test[Encoding(test) == "unknown"] 
+Encoding(test) <- "UTF-8"  
 
-
-for(i in (6570:10000)){
-  
-  url_pic <-glue("https://belgianfootball.s3.eu-central-1.amazonaws.com/s3fs-public/rbfa/img/players/internationals/football/men/{i}.jpg")
-  
-  res <- try(download.file(url_pic, destfile = glue("./content/post/data/190417down_1/{i}.jpg"), mode = "wb"))
-  if(inherits(res, "try-error"))
-  {
-    
-    next
-  }
-  download.file(url_pic, destfile = glue("./content/post/data/190417down_1/{i}.jpg"), mode = "wb")
-  
-  }
-
-
-
-for(i in (1:10000)){
-  
-  url_pic <-glue("https://belgianfootball.s3.eu-central-1.amazonaws.com/s3fs-public/rbfa/img/players/internationals/football/women/{i}.jpg")
-  
-  res <- try(download.file(url_pic, destfile = glue("./content/post/data/190417down_2/{i}.jpg"), mode = "wb"))
-  if(inherits(res, "try-error"))
-  {
-    
-    next
-  }
-  download.file(url_pic, destfile = glue("./content/post/data/190417down_2/{i}.jpg"), mode = "wb")
-  
-}
+names(test)
